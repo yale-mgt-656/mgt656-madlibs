ab6a0c99c9b30a17ed4e718b642de41d0cd0835b
Kyle L. Jensen <kljensen@gmail.com>
2015-09-29 20:58:40 -0400

    Simplify code a bit

--- server.js
+++ server.js
@@ -8,35 +8,17 @@ nunjucks.configure('stories', {
     express: app
 });
 
-var requireAll = require('./lib/misc.js').requireAll;
-var requireLayout = require('./lib/misc.js').requireLayout;
+var context = require('./lib/misc.js').getPOS(__dirname, 'pos');
 
-function loadPartOfSpeech(part){
-  var partsArray = requireAll(__dirname, part, part);
-  return function(){
-    // console.log('partsArray =', partsArray);
-    return _.sample(partsArray);
-  };
-}
-
-var partsOfSpeech = ['nouns', 'verbs', 'adjectives', 'adverbs'];
-var context = {};
-_.each(partsOfSpeech, function(part){
-  context[part] = {get: loadPartOfSpeech(part)};
-});
-
-// Get an array of all filenames.
-var allLayouts = requireLayout(__dirname, 'stories');
-// Set the origin into a random route.
+// When no template is specified, choose one randomly
 app.get('/', function(req, res) {
+  var allLayouts = require('./lib/misc.js').requireLayout(__dirname, 'stories');
   res.render(_.sample(allLayouts), context);
 });
-// Loop through each of the files in stories and render them.
-_.each(allLayouts, function(layout) {
-  var newlayout = layout.replace(".html", "");
-  app.get('/' + newlayout, function (req, res) {
-    res.render(layout, context);
-  });
+
+// Route to a particular template if one is specified
+app.get('/:template', function(req, res) {
+  res.render(req.params.template, context);
 });
 
 var server = app.listen(process.env.PORT, function () {
