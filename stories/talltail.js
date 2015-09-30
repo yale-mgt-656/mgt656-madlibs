dbfb94070c383a5101c34be641918679fa59d3c5
Noah <noah.aptekar@yale.edu>
2015-09-30 09:09:10 -0400

    story time

---
+++ stories/talltail.js
@@ -0,0 +1,19 @@
+<html>
+    <body>
+        <h1> {{adjectives.get()}} {{nouns.get()}}: A {{adjectives.get()}} Story For {{nouns.get()}} </h1>
+        <p> In the {{adjectives.get()}} beginning, there were {{advectives.get()}} {{nouns.get()}}. 
+	Seven times each day, the {{advectives.get()}} {{nouns.get()}} {{verbs.get()}}ed {{adverbs.get()}}.
+	It was {{adjectives.get()}}. Daytime. Nighttime.  {{nouns.get()}} were always {{verbs.get()}}ing.
+	
+	whoa... </p>
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
